a5d97cb0a022a76c118750a3374a5c413666a813
mko_io <me@mko.io>
2025-04-07 08:32:32 +1200

    Change the color

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -214,7 +214,7 @@ const Header = () => {
 
                   <Link
                     href="/auth/signup"
-                    className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho"
+                    className="flex items-center justify-center rounded-full bg-[rgb(199,155,68)] px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-[rgb(169,132,58)]"
                   >
                     Sign Up
                   </Link>
