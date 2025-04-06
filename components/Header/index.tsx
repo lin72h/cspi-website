e1e3268e5ed2c2bcfe77ceb96ab3e3cdff69e760
mko_io <me@mko.io>
2025-04-07 08:44:56 +1200

    Contact us

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -206,17 +206,10 @@ const Header = () => {
               ) : (
                 <>
                   <Link
-                    href="/auth/signin"
-                    className="text-regular font-medium text-waterloo hover:text-primary"
-                  >
-                    Sign In
-                  </Link>
-
-                  <Link
-                    href="/auth/signup"
+                    href="/support"
                     className="flex items-center justify-center rounded-full bg-cspi px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-cspiHover"
                   >
-                    Sign Up
+                    Contact Us
                   </Link>
                 </>
               )}
