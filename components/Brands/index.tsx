39ed73ac3e318ee1fd4c1b6e0a92ef4fe7a2e3e3
mko_io <me@mko.io>
2025-04-07 09:21:54 +1200

    update

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -9,7 +9,6 @@ const Brands = () => {
       {/* <!-- ===== Clients Start ===== --> */}
       <section className="border border-x-0 border-y-stroke bg-gray-400 py-16 dark:border-y-strokedark dark:bg-black">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
-          <h3 className="mb-10 text-center text-lg font-medium text-black dark:text-white">Trusted by Leading Companies</h3>
           <div className="grid grid-cols-1 items-center justify-center gap-10 md:grid-cols-3 lg:gap-20">
             {brandData.map((brand, key) => (
               <SingleBrand 
