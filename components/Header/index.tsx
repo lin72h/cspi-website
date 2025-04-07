82643a03989c080ad3b97f9659abf9f5c2d6b8b1
mko_io <me@mko.io>
2025-04-07 12:59:54 +1200

    update

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import ThemeToggler from "./ThemeToggler";
 import LanguageToggler from "./LanguageToggler";
 import menuData from "./menuData";
 import GlobalSearchModal from "../GlobalSearch";
@@ -206,7 +205,6 @@ const Header = () => {
               </button>
 
               <LanguageToggler />
-              <ThemeToggler />
 
               {session ? (
                 <>
